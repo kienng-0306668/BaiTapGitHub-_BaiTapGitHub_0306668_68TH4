baae5815754e820cb761350228c42daeba10385f
luancutee2k5 <ntluan231005@gmail.com>
2026-01-29 20:40:52 +0700

    code loginview

--- BaiTapGitHub _BaiTapGitHub_0306668_68TH4/MVVM/ViewModel/LoginViewModels.cs
+++ BaiTapGitHub _BaiTapGitHub_0306668_68TH4/MVVM/ViewModel/LoginViewModels.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace BaiTapGitHub__BaiTapGitHub_0306668_68TH4.MVVM.ModelView
 {
     internal class LoginViewModels
     {
+        public string Username {  get; set; }
+        public string Password { get; set; }
     }
 }
