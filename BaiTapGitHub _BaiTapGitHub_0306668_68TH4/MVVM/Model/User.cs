439bbe41b665803fb712cd4e265fb08f0988df76
kienng-0306668 <bluboy0303@gmail.com>
2026-01-29 21:08:15 +0700

    Thêm code model

--- BaiTapGitHub _BaiTapGitHub_0306668_68TH4/MVVM/Model/User.cs
+++ BaiTapGitHub _BaiTapGitHub_0306668_68TH4/MVVM/Model/User.cs
@@ -8,5 +8,9 @@ namespace BaiTapGitHub__BaiTapGitHub_0306668_68TH4.MVVM.Model
 {
     internal class User
     {
+        public int Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+
     }
 }
