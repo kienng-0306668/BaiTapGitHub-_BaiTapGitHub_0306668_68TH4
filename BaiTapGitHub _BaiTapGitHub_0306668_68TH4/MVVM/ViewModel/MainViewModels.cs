f64886ae0b0eb0951881a066087911b9d1f4399f
kienng-0306668 <bluboy0303@gmail.com>
2026-01-29 20:24:16 +0700

    Add project files.

---
+++ BaiTapGitHub _BaiTapGitHub_0306668_68TH4/MVVM/ViewModel/MainViewModels.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiTapGitHub__BaiTapGitHub_0306668_68TH4.MVVM.ViewModel
+{
+    internal class MainViewModels
+    {
+    }
+}
