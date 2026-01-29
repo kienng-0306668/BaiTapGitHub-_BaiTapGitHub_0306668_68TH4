6f6cfa20e3d3c46781260c7c8696df5605ab7d7c
luancutee2k5 <ntluan231005@gmail.com>
2026-01-29 21:10:16 +0700

    code lại

--- BaiTapGitHub _BaiTapGitHub_0306668_68TH4/MVVM/ViewModel/MainViewModels.cs
+++ BaiTapGitHub _BaiTapGitHub_0306668_68TH4/MVVM/ViewModel/MainViewModels.cs
@@ -9,6 +9,6 @@ namespace BaiTapGitHub__BaiTapGitHub_0306668_68TH4.MVVM.ViewModel
     internal class MainViewModels
     { 
         public string Title {  get; set; }= "Trang chủ";
-            public string StatusMessage { get; set; }
+         
     }
 }
